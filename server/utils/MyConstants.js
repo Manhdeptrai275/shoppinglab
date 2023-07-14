3dc810a8f7da96788466e3b371fe3a94a558407b
Manhdeptrai275 <93934639+Manhdeptrai275@users.noreply.github.com>
2023-07-14 21:12:38 +0700

    Add files via upload

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+    DB_SERVER: 'cluster0.tqzrhla.mongodb.net',
+    DB_USER: 'ducmanhhoangthieu',
+    DB_PASS: 'Camonban123',
+    DB_DATABASE: 'shopping',
+    EMAIL_USER: 'ducmanhhoangthieu@gmail.com', // Microsoft mail service
+    EMAIL_PASS: 'hzudbtmnpbiuaaeb',
+    JWT_SECRET: '<ducmanh123>',
+    JWT_EXPIRES: '3600000', // in milliseconds
+  };
+  module.exports = MyConstants;
